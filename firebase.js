44a4469fe68557e885d975db691eee673d9958fa
Bizslove AI <asbhatti363@gmail.com>
2025-11-08 12:38:48 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBuYXfr-_q-BJskWHN4chB8SZqMc-P9odw",
   authDomain: "toolnest-8e42d.firebaseapp.com",
@@ -12,6 +13,9 @@ const firebaseConfig = {
   appId: "1:988256036107:web:90f7e40f07593dff8e2e0e"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Auth & Firestore
 export const auth = getAuth(app);
 export const db = getFirestore(app);
